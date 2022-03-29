80cdc952924d188ebaaa13a8817fc6ee9936e3a4
architsingh5 <architsingh5@gmail.com>
2022-03-29 19:08:33 +0530

    did some changes

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
-import os, sys
+import os
+import sys
+from xml.etree.ElementTree import tostring
 import traci
+import random
 
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
@@ -12,86 +15,113 @@ sumo_cmd = [sumo_binary, "-c", "junction.sumocfg", "--start"]
 
 traci.start(sumo_cmd)
 
-west_east_lanes = ["E0_0", "-E1_0"]
-north_south_lanes = ["E2_0", "-E3_0"]
+# west_east_lanes = ["E0_0", "-E1_0"]
+# north_south_lanes = ["E2_0", "-E3_0"]
+
+lanes = ["E0", "E1", "E2", "E3"]
 
 trafficLightID = traci.trafficlight.getIDList()[0]
 
 step = 0
 total_vehicle_waiting_time = 0
+total_no_of_vehicles_crossed = 0
 
+traci.route.add("route01", ["E0", "E3"])
 
-def current_moving_lane(trafficLightID):
-    if traci.trafficlight.getRedYellowGreenState(trafficLightID).startswith("rrrr"):
-        return "WE"
-    else:
-        return "NS"
+routes = ["route01","route02","route03","route04","route05","route06","route07","route08","route09","route10","route11","route12"]
 
-
-def get_lane_lists(traffic_lanes_in_x_axis, traffic_lanes_in_y_axis, trafficLightID):
-    """
-    :param traffic_lanes_in_x_axis:  list
-        lanes controlled by traffic light in the x-axis
-    :param traffic_lanes_in_y_axis: list
-        lanes controlled by traffic light in the y-axis
-    :return: green_light_lanes: array
-        list of lanes currently moving.
-    ::return: red_light_lanes: array
-        list of lanes blocked by traffic
-    """
-    if current_moving_lane(trafficLightID) == "WE":
-        return traffic_lanes_in_x_axis, traffic_lanes_in_y_axis
-    else:
-        return traffic_lanes_in_y_axis, traffic_lanes_in_x_axis
-
-
-def get_vehicles_in_lanes(lane_ids):
-    """
-
-    :param array_of_lane_id: list
-        a list containing the IDs of the lanes to get vehicles from
-    :return: vehicles: list
-        a list of all vehicles in the lanes
-
-    """
-    vehicles = []
-    for lane_id in lane_ids:
-        vehicles = vehicles + list(traci.lane.getLastStepVehicleIDs(lane_id))
-    return vehicles
-
-
-def vehicle_waiting_time_in_lane(vehicle_list):
-    waiting_times = []
-    for vehicle in vehicle_list:
-        waiting_times.append(traci.vehicle.getAccumulatedWaitingTime(vehicle))
-    if len(waiting_times) == 0:
-        return 0
-    else:
-        return waiting_times
-
-
-while step < 5000:
-    lanes_currently_moving, lanes_stopped_by_light = get_lane_lists(
-        west_east_lanes, north_south_lanes, trafficLightID
-    )
-
-    vehicles_in_red_lanes = get_vehicles_in_lanes(lanes_stopped_by_light)
-    vehicles_in_green_lanes = get_vehicles_in_lanes(lanes_currently_moving)
-
-    no_vehicles_in_red_lanes = len(vehicles_in_red_lanes)
-    no_vehicles_in_green_lanes = len(vehicles_in_green_lanes)
-
-    vehicles_waiting_time = vehicle_waiting_time_in_lane(vehicles_in_red_lanes)
-
-    if vehicles_waiting_time != 0:
-        vehicles_waiting_time.sort()
-        max_waiting_time_in_red_lanes = vehicles_waiting_time[-1]
-        sum_wt_time = sum(vehicles_waiting_time)
-        total_vehicle_waiting_time += sum_wt_time
-
+while step < 1000:
+    # print(traci.trafficlight.getRedYellowGreenState(trafficLightID))
+    # print(traci.trafficlight.getPhaseDuration(trafficLightID))
+    vechile_id = "vehicle_" + str(step)
+    # traci.vehicle.add(vechile_id, random.choice(routes))
     traci.simulationStep()
     step += 1
 
-print(total_vehicle_waiting_time)
-
+# traci.simulationStep()
+    
 traci.close()
+
+
+# def current_moving_lane(trafficLightID):
+#     if traci.trafficlight.getRedYellowGreenState(trafficLightID).startswith("rrrr"):
+#         return "WE"
+#     else:
+#         return "NS"
+
+
+# def get_lane_lists(traffic_lanes, trafficLightID):
+#     """
+#     :param traffic_lanes_in_x_axis:  list
+#         lanes controlled by traffic light in the x-axis
+#     :param traffic_lanes_in_y_axis: list
+#         lanes controlled by traffic light in the y-axis
+#     :return: green_light_lanes: array
+#         list of lanes currently moving.
+#     ::return: red_light_lanes: array
+#         list of lanes blocked by traffic
+#     """
+
+# print(traci.trafficlight.getRedYellowGreenState(trafficLightID))
+# print(traci.trafficlight.getPhaseDuration(trafficLightID))
+
+# if current_moving_lane(trafficLightID) == "WE":
+#     return traffic_lanes_in_x_axis, traffic_lanes_in_y_axis
+# else:
+#     return traffic_lanes_in_y_axis, traffic_lanes_in_x_axis
+
+
+# def get_vehicles_in_lanes(lane_ids):
+#     """
+
+#     :param array_of_lane_id: list
+#         a list containing the IDs of the lanes to get vehicles from
+#     :return: vehicles: list
+#         a list of all vehicles in the lanes
+
+#     """
+#     vehicles = []
+#     for lane_id in lane_ids:
+#         vehicles = vehicles + list(traci.lane.getLastStepVehicleIDs(lane_id))
+#     return vehicles
+
+
+# def vehicle_waiting_time_in_lane(vehicle_list):
+#     waiting_times = []
+#     for vehicle in vehicle_list:
+#         waiting_times.append(traci.vehicle.getAccumulatedWaitingTime(vehicle))
+#     if len(waiting_times) == 0:
+#         return 0
+#     else:
+#         return waiting_times
+
+# while step < 1000:
+# print(traci.trafficlight.getRedYellowGreenState(trafficLightID))
+# print(traci.trafficlight.getPhaseDuration(trafficLightID))
+
+# lanes_currently_moving, lanes_stopped_by_light = get_lane_lists(
+#     lanes, trafficLightID
+# )
+
+# get_lane_lists(lanes, trafficLightID)
+
+# vehicles_in_red_lanes = get_vehicles_in_lanes(lanes_stopped_by_light)
+# vehicles_in_green_lanes = get_vehicles_in_lanes(lanes_currently_moving)
+
+# no_vehicles_in_red_lanes = len(vehicles_in_red_lanes)
+# no_vehicles_in_green_lanes = len(vehicles_in_green_lanes)
+
+# vehicles_waiting_time = vehicle_waiting_time_in_lane(vehicles_in_red_lanes)
+
+# if vehicles_waiting_time != 0:
+#     vehicles_waiting_time.sort()
+#     max_waiting_time_in_red_lanes = vehicles_waiting_time[-1]
+#     sum_wt_time = sum(vehicles_waiting_time)
+#     total_vehicle_waiting_time += sum_wt_time
+
+# traci.simulationStep()
+# step += 1
+
+# print(total_vehicle_waiting_time)
+
+# traci.close()
