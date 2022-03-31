2307d3e58554d9f372f68254477c5caef81f26fa
architsingh5 <architsingh5@gmail.com>
2022-03-31 16:42:57 +0530

    added functions

--- script.py
+++ script.py
@@ -15,7 +15,13 @@ sumo_cmd = [sumo_binary, "-c", "junction.sumocfg", "--start"]
 # routes = ["route01","route02","route03","route04","route05","route06","route07","route08","route09","route10","route11","route12"]
 # lanes = ["E0", "E1", "E2", "E3"]
 # trafficLightID = traci.trafficlight.getIDList()[0]
-# traci.route.add("route01", ["E0", "E3"])
+# traci.
+# route.add("route01", ["E0", "E3"])
+
+lane=0
+
+def GreenSignaltime(no_of_vehicles_in_lane,no_of_vehicles_in_other_queue,maximum_waiting_time):
+    return 30
 
 def static_tls():
     traci.start(sumo_cmd)
@@ -24,15 +30,66 @@ def static_tls():
     total_no_of_vehicles_crossed = 0
 
     while step < 1000:
+        
+        if(lane==0):
+            lane_id = "E2"
+            lane = 1
+            no_of_vehicles = traci.lane.getLastStepVehicleIDs(lane_id)
+            no_of_vehicles_other = traci.lane.getLastStepVehicleIDs("-E1")
+            no_of_vehicles_other+= traci.lane.getLastStepVehicleIDs("-E3")
+            no_of_vehicles_other+= traci.lane.getLastStepVehicleIDs("E0")
 
-        # print(traci.trafficlight.getRedYellowGreenState(trafficLightID))
-        # print(traci.trafficlight.getPhaseDuration(trafficLightID))
-        # vechile_id = "vehicle_" + str(step)
-        # traci.vehicle.add(vechile_id, random.choice(routes))
+            vehicles_on_lane = traci.edge.getLastStepVehicleIDs("E2")
+            first_vehicle = vehicles_on_lane[0]
+            first_vehicle_waiting_time = traci.vehicle.getWaitingTime(first_vehicle)
+
+            gst = GreenSignaltime(no_of_vehicles,no_of_vehicles_other,first_vehicle_waiting_time)
+
+            traci.trafficlight.setPhase("J2",)
+
+        # elif(lane==1):
+        #     lane_id = "-E1"
+        #     lane = 2
+        #     no_of_vehicles = traci.lane.getLastStepVehicleIDs(lane_id)
+        #     no_of_vehicles_other = traci.lane.getLastStepVehicleIDs("E2")
+        #     no_of_vehicles_other+= traci.lane.getLastStepVehicleIDs("-E3")
+        #     no_of_vehicles_other+= traci.lane.getLastStepVehicleIDs("E0")
+
+        #     vehicles_on_lane = traci.edge.getLastStepVehicleIDs("-E1")
+        #     first_vehicle = vehicles_on_lane[0]
+        #     first_vehicle_waiting_time = traci.vehicle.getWaitingTime(first_vehicle)
+
+        #     gst = GreenSignaltime(no_of_vehicles,no_of_vehicles_other,first_vehicle_waiting_time)
+
+
+            
+    
         traci.simulationStep()
         step += 1
 
     traci.close()
+
+# print(traci.trafficlight.getRedYellowGreenState(trafficLightID))
+# print(traci.trafficlight.getPhaseDuration(trafficLightID))
+# vechile_id = "vehicle_" + str(step)
+# traci.vehicle.add(vechile_id, random.choice(routes))
+
+def get_vehicles_in_lane(array_of_lane_id):
+    vehicles = []
+    for laneIDs in array_of_lane_id:
+        vehicles = vehicles + list(traci.lane.getLastStepVehicleIDs(laneIDs))
+    return vehicles
+
+
+def vehicle_waiting_time_in_lane(vehicle_list):
+    waiting_times = []
+    for vehicle in vehicle_list:
+        waiting_times.append(traci.vehicle.getAccumulatedWaitingTime(vehicle))
+    if len(waiting_times) == 0:
+        return 0
+    else:
+        return waiting_times
+
 
 def dynamic_tls():
     pass
