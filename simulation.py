50927d84de698a2370a1f5d825d737d762795d9a
Archit Singh <architsingh5@gmail.com>
2023-03-01 09:06:26 +0530

    Updated simulation.py to calculate improvement

--- simulation.py
+++ simulation.py
@@ -3,7 +3,15 @@ from simulation_dynamic import dynamic_tls
 
 if __name__ == "__main__":
     print("===STATIC===")
-    static_tls()
+    a1,b1,c1,d1 = static_tls()
     print()
     print("===DYNAMIC===")
-    dynamic_tls()
+    a2,b2,c2,d2 = dynamic_tls()
+    print()
+    print("===PERCENTAGE IMPROVEMENT===",)
+    print("Total Number of Vehicles Crossed",round((a2-a1)/a1*100,2),"%")
+    print("Average Waiting Time",round((b1-b2)/b1*100,2),"%")
+    # print("Total CO2 Emission",round((c1-c2)/c1*100,2),"%")
+    # print("Total Fuel Consumption",round((d1-d2)/d1*100,2),"%")
+    print()
+
