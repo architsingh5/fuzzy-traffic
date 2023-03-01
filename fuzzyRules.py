55d50d00d251c107118d4642b3e6288bd4dbe977
Archit Singh <architsingh5@gmail.com>
2023-03-01 09:15:10 +0530

    Back to old membership functions

--- fuzzyRules.py
+++ fuzzyRules.py
@@ -3,10 +3,10 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-max_queue_length_curr = 80
-max_queue_length_other = 240
-max_waiting_time = 270
+max_queue_length_curr = 60
 max_green_signal_time = 60
+max_queue_length_other = max_queue_length_curr * 3
+max_waiting_time = max_green_signal_time * 3
 a = max_queue_length_curr/5
 b = max_queue_length_other/5
 c = max_waiting_time/5
@@ -20,68 +20,36 @@ green_signal_time = ctrl.Consequent(np.arange(0, max_green_signal_time+1, 1), "g
 
 level = ["low", "med", "high"]
 
-# queue_length_curr.automf(3, names=level)
-# queue_length_other.automf(3, names=level)
-# max_waiting_time.automf(3, names=level)
+queue_length_curr.automf(3, names=level)
+queue_length_other.automf(3, names=level)
+max_waiting_time.automf(3, names=level)
 
-queue_length_curr['low'] = fuzz.trapmf(queue_length_curr.universe ,  [0, 0, a, a*2] )
-queue_length_curr['med'] = fuzz.trapmf(queue_length_curr.universe ,  [a, a*2, a*3, a*4] )
-queue_length_curr['high'] = fuzz.trapmf(queue_length_curr.universe ,  [a*3, a*4, a*5, a*5] )
+# queue_length_curr['low'] = fuzz.trapmf(queue_length_curr.universe ,  [0, 0, 0, a*2] )
+# queue_length_curr['med'] = fuzz.trapmf(queue_length_curr.universe ,  [a, a*2, a*3, a*4] )
+# queue_length_curr['high'] = fuzz.trapmf(queue_length_curr.universe ,  [a*3, a*4, a*5, a*5] )
 
-queue_length_other['low'] = fuzz.trapmf(queue_length_other.universe ,  [0, 0, b, b*2] )
-queue_length_other['med'] = fuzz.trapmf(queue_length_other.universe ,  [b, b*2, b*3, b*4] )
-queue_length_other['high'] = fuzz.trapmf(queue_length_other.universe ,  [b*3, b*4, b*5, b*5] )
+# queue_length_other['low'] = fuzz.trapmf(queue_length_other.universe ,  [0, 0, 0, b*2] )
+# queue_length_other['med'] = fuzz.trapmf(queue_length_other.universe ,  [b, b*2, b*3, b*4] )
+# queue_length_other['high'] = fuzz.trapmf(queue_length_other.universe ,  [b*3, b*4, b*5, b*5] )
 
-max_waiting_time['low'] = fuzz.trapmf(max_waiting_time.universe ,  [0, 0, c, c*2] )
-max_waiting_time['med'] = fuzz.trapmf(max_waiting_time.universe ,  [c, c*2, c*3, c*4] )
-max_waiting_time['high'] = fuzz.trapmf(max_waiting_time.universe ,  [c*3, c*4, c*5, c*5] )
+# max_waiting_time['low'] = fuzz.trapmf(max_waiting_time.universe ,  [0, 0, 0, c*2] )
+# max_waiting_time['med'] = fuzz.trapmf(max_waiting_time.universe ,  [c, c*2, c*3, c*4] )
+# max_waiting_time['high'] = fuzz.trapmf(max_waiting_time.universe ,  [c*3, c*4, c*5, c*5] )
 
 # queue_length_curr.view()
 # queue_length_other.view()
 # max_waiting_time.view()
 
-# green_signal_time.automf(3, names=level)
+green_signal_time.automf(3, names=level)
 
-green_signal_time['low'] = fuzz.trapmf(green_signal_time.universe ,  [0, 0, d, d*2] )
-green_signal_time['med'] = fuzz.trapmf(green_signal_time.universe ,  [d, d*2, d*3, d*4] )
-green_signal_time['high'] = fuzz.trapmf(green_signal_time.universe ,  [d*3, d*4, d*5, d*5] )
+# green_signal_time['low'] = fuzz.trapmf(green_signal_time.universe ,  [0, 0, 0, d*2] )
+# green_signal_time['med'] = fuzz.trapmf(green_signal_time.universe ,  [d, d*2, d*3, d*4] )
+# green_signal_time['high'] = fuzz.trapmf(green_signal_time.universe ,  [d*3, d*4, d*5, d*5] )
 
 # green_signal_time.view()
 
 rule1 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["low"] & max_waiting_time["low"],
-    green_signal_time["low"],
-)
-rule2 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["low"] & max_waiting_time["med"],
-    green_signal_time["low"],
-)
-rule3 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["low"] & max_waiting_time["high"],
-    green_signal_time["low"],
-)
-rule4 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["med"] & max_waiting_time["low"],
-    green_signal_time["low"],
-)
-rule5 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["med"] & max_waiting_time["med"],
-    green_signal_time["low"],
-)
-rule6 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["med"] & max_waiting_time["high"],
-    green_signal_time["low"],
-)
-rule7 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["high"] & max_waiting_time["low"],
-    green_signal_time["low"],
-)
-rule8 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["high"] & max_waiting_time["med"],
-    green_signal_time["low"],
-)
-rule9 = ctrl.Rule(
-    queue_length_curr["low"] & queue_length_other["high"] & max_waiting_time["high"],
+    queue_length_curr["low"],
     green_signal_time["low"],
 )
 rule10 = ctrl.Rule(
@@ -161,14 +129,6 @@ rule27 = ctrl.Rule(
 GST_control = ctrl.ControlSystem(
     [
         rule1,
-        rule2,
-        rule3,
-        rule4,
-        rule5,
-        rule6,
-        rule7,
-        rule8,
-        rule9,
         rule10,
         rule11,
         rule12,
@@ -212,3 +172,4 @@ if __name__ == "__main__":
     max_waiting_time.view()
     green_signal_time.view()
     plt.show()
+    # print(fuzzy_controller_function(0, 0, 0))
